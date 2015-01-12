6d07d9d9d1221ce8f7a215ec27dad9e5bc97d493
Mauro Otonelli <mauro.otonelli@gmail.com>
2015-01-11 23:26:15 -0300

    First version of the conversion rate script.

---
+++ conversion_rate_for_actors.rb
@@ -0,0 +1,44 @@
+require 'rainbow/ext/string'
+require 'optparse'
+require 'wolfram-alpha'
+
+@options = {}
+
+OptionParser.new do |opts|
+  opts.on("--actors a,b,c", Array, "Array of actors") do |actors|
+    @options[:actors] = actors
+  end
+end.parse!
+
+class OscarConversionRate
+  def initialize
+    options = { "format" => "plaintext" }
+    @client = WolframAlpha::Client.new ENV["WOLFRAM_ALPHA_API_KEY"], options
+  end
+
+  def conversion_rate_for(actors)
+    puts "Calculating conversion rate... "
+    actors.each do |actor|
+      nominations, wins = count_nominations_and_wins_for(actor)
+      nominations += wins 
+      result = wins.to_f / nominations
+      result = 1 if result.infinite?
+      puts "#{titleize(actor).color(:green)}: #{result.round(4).to_s.color(:cyan)} (Wins: #{wins.to_s.color(:red)}, Nominations: #{nominations.to_s.color(:magenta)})"
+    end
+    puts "Finished! "
+  end
+
+  def count_nominations_and_wins_for(actor)
+    response = @client.query "#{actor} oscar nominations"
+    nominations = response.pods.last.subpods.first
+    result = nominations.plaintext
+    [result.scan(/nominee/).count, result.scan(/winner/).count]
+  end
+
+  def titleize(str)
+    str.split(/ |\_/).map(&:capitalize).join(" ")
+  end
+end
+
+oscars = OscarConversionRate.new
+puts oscars.conversion_rate_for(@options[:actors])
